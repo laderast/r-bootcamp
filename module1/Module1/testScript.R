2d06ad950b1928d7ddf516508fce2f5825866b0f
laderast <roshioshi@gmail.com>
2014-07-07 12:43:58 -0700

    removed bad dir

--- module1/Module1/testScript.R
+++
@@ -1,7 +0,0 @@
-##load iris data
-data(iris)
-##save pdf of boxplot of Sepal Length and Sepal Width (the first two columns)
-pdf("iris-boxplots.pdf")
-boxplot(iris[,1:2], "Sepal Properties of Iris Dataset")
-dev.off()
-
