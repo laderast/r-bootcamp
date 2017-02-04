9bdec4dabd1d54ed958810aaec8089a4fe03b0b8
laderast <roshioshi@gmail.com>
2017-02-03 16:14:08 -0800

    added error back in module1/testScript.R

--- module1/testScript.R
+++ module1/testScript.R
@@ -2,6 +2,6 @@
 data(iris)
 ##save pdf of boxplot of Sepal Length and Sepal Width (the first two columns)
 pdf("iris-boxplots.pdf")
-boxplot(iris[,1:2], "Sepal Properties of Iris Dataset")
+boxplot(iris[,1:2], "Sepal Properties of Iris Dataset)
 dev.off()
 
